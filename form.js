cc62f6ca7addc7b93e7b24c4926910fe0ba58ffd
VadaszDaniel2006 <vadaszdana@gmail.com>
2025-04-10 12:46:00 +0200

    Add files via upload

---
+++ form.js
@@ -0,0 +1,12 @@
+
+//AZ űrlap elérése,eseménykezelő hozzáadása, esemény objektum hozzáadása
+document.getElementById('userForm').addEventListener('submit', (e) =>{
+    //Űrlap viselkedés letiltása(csak adat forrás)
+    e.preventDefault();
+//Fromdata objektum létrehozása, az űrlap-esemény hozzáadása 
+    const myFromData = new FormData(e.target);
+    //Az űrlap adatait tároló objektum létrehozása    
+    const userData = Object.fromEntries(myFromData);
+    console.log(userData);
+
+})
